bb9c3e43b638e0bef8815aeaf69335b0c0c96482
Emmanuel Falola <emmanuelfalola9998@gmail.com>
2022-06-05 16:52:52 +0100

    added formatting

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,119 +1,186 @@
-import { useState, useEffect } from 'react'
-import axios from "axios"
-import { getTeamsInterface } from '../utils/types'
-import { getTeamInfoInterface } from '../utils/types'
-import {getPlayersInterface} from '../utils/types'
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { getTeamsInterface } from "../utils/types";
+import { getTeamInfoInterface } from "../utils/types";
+import { getPlayersInterface } from "../utils/types";
 
 export function MainComponent(): JSX.Element {
-    const [id, setID] = useState(0)
-    const [searchTerm, setSearchTerm] = useState<string>("")
-    const [getTeams, setGetTeams] = useState<getTeamsInterface[]>([])
-    const [teamInfo, setTeamInfo] = useState<getTeamInfoInterface>(
-        {id:0,
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [getTeams, setGetTeams] = useState<getTeamsInterface[]>([]);
+  const [teamInfo, setTeamInfo] = useState<getTeamInfoInterface>({
+    id: 0,
+    abbreviation: "",
+    city: "",
+    conference: "",
+    division: "",
+    full_name: "",
+    name: "",
+  });
+  const [allPlayers, getAllPlayers] = useState<getPlayersInterface[]>([]);
+  const [playerInfo, getPlayerInfo] = useState<getPlayersInterface>({
+    id: 0,
+    first_name: "",
+    last_name: "",
+    position: "",
+    height_feet: 0,
+    height_inches: 0,
+    weight_pounds: 0,
+    team: {
+      id: 0,
+      abbreviation: "",
+      city: "",
+      conference: "",
+      division: "",
+      full_name: "",
+      name: "",
+    },
+  });
+
+  const apiLink = "https://balldontlie.io/api/v1/";
+
+  useEffect(() => {
+    async function getAllTeams() {
+      const response = await axios.get(apiLink + "teams");
+      setGetTeams(response.data.data);
+    }
+    getAllTeams();
+  }, []);
+
+  useEffect(() => {
+    async function getPlayers() {
+      const response = await axios.get(apiLink + "players/");
+      getAllPlayers(response.data.data);
+    }
+    getPlayers();
+  }, []);
+
+  const filteredPlayerNames = isSearchTerminPlayers(allPlayers, searchTerm);
+
+  function isSearchTerminPlayers(
+    getPlayers: getPlayersInterface[],
+    searchTerm: string
+  ) {
+    const playerSearch = getPlayers.filter(
+      (obj) =>
+        obj["first_name"].toLowerCase().includes(searchTerm.toLowerCase()) ||
+        obj["last_name"].toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return playerSearch;
+  }
+
+  return (
+    <>
+      <h3> Welcome to the NBA </h3>
+
+      {getTeams.map((team) => (
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => setTeamInfo(team)}
+          key={team.id}
+        >
+          {team.id} {team.full_name}
+        </button>
+      ))}
+      {teamInfo.id !== 0 && (
+        <ul className="list-group">
+          <li className="list-group-item">{teamInfo.abbreviation}</li>
+          <li className="list-group-item">{teamInfo.city}</li>
+          <li className="list-group-item">{teamInfo.conference}</li>
+          <li className="list-group-item">{teamInfo.division}</li>
+          <li className="list-group-item">{teamInfo.full_name}</li>
+          <li className="list-group-item">{teamInfo.name}</li>
+        </ul>
+      )}
+      <br />
+      <button
+        onClick={() =>
+          setTeamInfo({
+            id: 0,
             abbreviation: "",
             city: "",
             conference: "",
             division: "",
             full_name: "",
-            name: ""}
-    )
-    const [allPlayers, getAllPlayers] = useState<getPlayersInterface[]>([])
-    const [playerInfo, getPlayerInfo] = useState<getPlayersInterface>({
-        id:0,
-        first_name:'',
-        last_name:'',
-        position:'',
-        height_feet: 0,
-        height_inches: 0,
-        weight_pounds: 0,
-        team:{
-          id:0,
-          abbreviation:'',
-          city:'',
-          conference:'',
-          division:'',
-          full_name:'',
-          name:''
+            name: "",
+          })
         }
-    })
-
-
-    const apiLink = "https://balldontlie.io/api/v1/"
-
-    useEffect(() => {
-        async function getAllTeams() {
-            const response = await axios.get(apiLink + "teams")
-            setGetTeams(response.data.data)
-
+      >
+        Reset
+      </button>
+      <br />
+      <div>
+        <input
+          placeholder="Type NBA Player Name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        Displaying {filteredPlayerNames.length} out of {allPlayers.length}
+      </div>
+      {filteredPlayerNames.map((player) => (
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={() => getPlayerInfo(player)}
+          key={player.id}
+        >
+          {player.first_name} {player.last_name}
+        </button>
+      ))}
+      {playerInfo.id !== 0 && (
+        <ul className="list-group">
+          <li className="list-group-item">
+            ID: <b>{playerInfo.id}</b>
+          </li>
+          <li className="list-group-item">
+            First Name: <b>{playerInfo.first_name}</b>
+          </li>
+          <li className="list-group-item">
+            Last Name: <b>{playerInfo.last_name}</b>
+          </li>
+          <li className="list-group-item">
+            Height:{" "}
+            <b>
+              {playerInfo.height_feet}FT{playerInfo.height_inches}
+            </b>
+          </li>
+          <li className="list-group-item">
+            Position: <b>{playerInfo.position}</b>
+          </li>
+          <li className="list-group-item">
+            Team City and Abbv:{" "}
+            <b>
+              {playerInfo.team.city}, ({playerInfo.team.abbreviation})
+            </b>
+          </li>
+        </ul>
+      )}
+      <br />
+      <button
+        onClick={() =>
+          getPlayerInfo({
+            id: 0,
+            first_name: "",
+            last_name: "",
+            position: "",
+            height_feet: 0,
+            height_inches: 0,
+            weight_pounds: 0,
+            team: {
+              id: 0,
+              abbreviation: "",
+              city: "",
+              conference: "",
+              division: "",
+              full_name: "",
+              name: "",
+            },
+          })
         }
-        getAllTeams()
-    }, [])
-
-    useEffect(() => {
-        async function getPlayers() {
-            const response = await axios.get(apiLink + "players/" )
-            getAllPlayers(response.data.data)
-        }
-        getPlayers()
-
-    }, [id])
-
-   
-    const filteredPlayerNames = isSearchTerminPlayers(allPlayers, searchTerm)
-
-    function isSearchTerminPlayers(getPlayers: getPlayersInterface[],searchTerm: string) {
-    const playerSearch = getPlayers.filter(
-        (obj) =>
-        obj["first_name"].toLowerCase().includes(searchTerm.toLowerCase()) ||
-        obj["last_name"].toLowerCase().includes(searchTerm.toLowerCase())
-    )
-
-    return playerSearch;
-    }
-
-
-
-
-
-
-
-    return (
-        <>
-            <h3> Welcome to the NBA   </h3>
-
-            {getTeams.map((team) => (<button type="button" className="btn btn-primary" onClick={() => setTeamInfo(team)} key={team.id}>{team.id} {team.full_name}</button>))}
-            {teamInfo.id !== 0 &&<ul className='list-group'>
-                <li className='list-group-item'>{teamInfo.abbreviation}</li>
-                <li className='list-group-item'>{teamInfo.city}</li>
-                <li className='list-group-item'>{teamInfo.conference}</li>
-                <li className='list-group-item'>{teamInfo.division}</li>
-                <li className='list-group-item'>{teamInfo.full_name}</li>
-                <li className='list-group-item'>{teamInfo.name}</li>
-            </ul>
-            }
-            <div>
-                <input 
-                    placeholder='Type NBA Player Name'
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                Displaying {filteredPlayerNames.length} out of {allPlayers.length}
-            </div>
-            {filteredPlayerNames.map((player) => (<button type='button' className= "btn btn-success" onClick={() => getPlayerInfo(player)} key={player.id}>{player.first_name}</button>))}
-            {playerInfo.id !==0 && <ul className='list-group'>
-                <li className='list-group-item'>{playerInfo.first_name}</li>
-                <li className='list-group-item'>{playerInfo.last_name}</li>
-                <li className='list-group-item'>{playerInfo.height_feet}</li>
-                <li className='list-group-item'>{playerInfo.height_inches}</li>
-                <li className='list-group-item'>{playerInfo.position}</li>
-                <li className='list-group-item'>{playerInfo.team.abbreviation}</li>
-                <li className='list-group-item'>{playerInfo.team.city}</li>
-                <li className='list-group-item'>{playerInfo.id}</li>
-            </ul>}
-            {console.log(allPlayers)}
-        </>
-    )
-
+      >
+        Reset
+      </button>
+    </>
+  );
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,25 +1,12 @@
-export interface getPlayersInterface{
-    
-        id:number,
-        first_name:string,
-        last_name:string,
-        position:string,
-        height_feet: number,
-        height_inches: number,
-        weight_pounds: number,
-        team:{
-          id:number,
-          abbreviation:string,
-          city:string,
-          conference:string,
-          division:string,
-          full_name:string,
-          name:string
-        }
-}
-
-
-export interface getTeamsInterface{
+export interface getPlayersInterface {
+  id: number;
+  first_name: string;
+  last_name: string;
+  position: string;
+  height_feet: number;
+  height_inches: number;
+  weight_pounds: number;
+  team: {
     id: number;
     abbreviation: string;
     city: string;
@@ -27,14 +14,25 @@ export interface getTeamsInterface{
     division: string;
     full_name: string;
     name: string;
+  };
+}
+
+export interface getTeamsInterface {
+  id: number;
+  abbreviation: string;
+  city: string;
+  conference: string;
+  division: string;
+  full_name: string;
+  name: string;
 }
 
 export interface getTeamInfoInterface {
-    id:number,
-    abbreviation: string
-    city: string,
-    conference: string,
-    division: string,
-    full_name: string,
-    name: string
+  id: number;
+  abbreviation: string;
+  city: string;
+  conference: string;
+  division: string;
+  full_name: string;
+  name: string;
 }
