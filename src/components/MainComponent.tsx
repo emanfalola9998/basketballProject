e8f413ab3ea65907026e7d3dc5a13ef1a36ba366
Emmanuel Falola <emmanuelfalola9998@gmail.com>
2022-06-05 14:14:31 +0100

    added conditional rendering and list CSS

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,22 +1,55 @@
 import { useState, useEffect } from 'react'
 import axios from "axios"
-import { getPlayersInterface } from '../utils/types'
+import { getTeamsInterface } from '../utils/types'
+import { getTeamInfoInterface } from '../utils/types'
 
 
-export function MainComponent(): JSX.Element{    
-    const [getPlayers, setGetPlayers] = useState<getPlayersInterface[]>([])
+
+
+export function MainComponent(): JSX.Element {
+    const [getTeams, setGetTeams] = useState<getTeamsInterface[]>([])
+    const [teamInfo, setTeamInfo] = useState<getTeamInfoInterface>(
+        {id:0,
+            abbreviation: "",
+            city: "",
+            conference: "",
+            division: "",
+            full_name: "",
+            name: ""}
+    )
+
+    const apiLink = "https://balldontlie.io/api/v1/"
 
     useEffect(() => {
-        async function getAllPlayers() {
-            const response = await axios.get("https://balldontlie.io/api/v1/players")
-            setGetPlayers(response.data)
+        async function getAllTeams() {
+            const response = await axios.get(apiLink + "teams")
+            setGetTeams(response.data.data)
+
         }
-        getAllPlayers()
-    },[])
-    
+        getAllTeams()
+    }, [])
+
+
+
+
+
+
+
+
     return (
         <>
-            
+            <h3> Welcome to the NBA   </h3>
+
+            {getTeams.map((team) => (<button type="button" className="btn btn-primary" onClick={() => setTeamInfo(team)} key={team.id}>{team.id} {team.full_name}</button>))}
+            {teamInfo.id !== 0 &&<ul className='list-group'>
+                <li className='list-group-item'>{teamInfo.abbreviation}</li>
+                <li className='list-group-item'>{teamInfo.city}</li>
+                <li className='list-group-item'>{teamInfo.conference}</li>
+                <li className='list-group-item'>{teamInfo.division}</li>
+                <li className='list-group-item'>{teamInfo.full_name}</li>
+                <li className='list-group-item'>{teamInfo.name}</li>
+            </ul>
+            }
         </>
     )
 
